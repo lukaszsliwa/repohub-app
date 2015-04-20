ede528656742727565716cec9ecbd8468c3ddda9
Łukasz Śliwa <lukasz.sliwa@apptamers.com>
2015-04-20 13:13:22 +0200

    Regenerated keys after commit

--- app/models/key.rb
+++ app/models/key.rb
@@ -6,9 +6,11 @@ class Key < ActiveRecord::Base
   validates :name, :value, presence: true
 
   def generate_authorized_keys
-    File.open("#{Rails.root}/public/authorized_keys/#{user.username}", "w") do |authorized_key|
-      user.keys.each do |key|
-        authorized_key.write "#key-#{key.id}\n#{key.value}\n\n"
+    #`echo "#{content}" > /home/#{user.username}/.ssh/authorized_keys`
+    File.open("/home/#{user.username}/.ssh/authorized_keys", 'w') do |file|
+      file.write "# Generated at #{DateTime.now}\n"
+      user.keys.all.map do |key|
+        file.write "# key-#{key.id}\n#{key.value}\n\n"
       end
     end
   end
--- bin/application/user/create.sh
+++ bin/application/user/create.sh
@@ -2,3 +2,4 @@
 
 echo "[server] Adding $1 user"
 sudo /usr/sbin/useradd -m -s /usr/bin/git-shell -b /home/$1 -c "Repohub $1" -d /home/$1 $1
+sudo setfacl -m user:$USER:rw- /home/$1/.ssh/authorized_keys
